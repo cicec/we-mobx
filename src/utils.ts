ffe65df47f81b97b2d1818d17dc76cb4ca8f6359
clancysong <clancysong@gmail.com>
2020-05-10 23:08:51 +0800

    feat: add support for map & set data type

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 export const is = {
   fun: (a: unknown): a is Function => typeof a === 'function',
   arr: (a: unknown): a is [] => Array.isArray(a),
-  obj: (a: unknown): a is AnyObject => Object.prototype.toString.call(a) === '[object Object]'
+  obj: (a: unknown): a is AnyObject => Object.prototype.toString.call(a) === '[object Object]',
+  set: (a: unknown): a is Set<any> => Object.prototype.toString.call(a) === '[object Set]',
+  map: (a: unknown): a is Map<any, any> => Object.prototype.toString.call(a) === '[object Map]',
 }
 
 export const toData = (source: any): any => {
@@ -9,11 +11,26 @@ export const toData = (source: any): any => {
     return source.map((item) => toData(item))
   }
 
+  if (is.set(source)) {
+    return Array.from(source).map((item) => toData(item))
+  }
+
   if (is.obj(source)) {
     const target: AnyObject = {}
 
     Object.getOwnPropertyNames(source).forEach((key) => {
       target[key] = toData(source[key])
+    })
+
+    return target
+  }
+
+  if (is.map(source)) {
+    const obj = Object.fromEntries(source)
+    const target: AnyObject = {}
+
+    Object.getOwnPropertyNames(obj).forEach((key) => {
+      target[key] = toData(obj[key])
     })
 
     return target
